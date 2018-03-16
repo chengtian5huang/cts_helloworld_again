0f8481f48b5bf4b8119fb97fae0a53c3d8ab1fb8
程天爽 <34744187+chengtian5huang@users.noreply.github.com>
2018-03-16 15:53:38 +0800

    one_clk
    add local auth avoid expose client if to public, though easy to crack.

--- one_click.sh
+++
@@ -1,2 +0,0 @@
-wget --user-agent="Mozilla/5.0 （Windows; U; Windows NT 6.1; en-US） AppleWebKit/534.16 （KHTML， like Gecko） Chrome/10.0.648.204 Safari/534.16" https://raw.githubusercontent.com/chengtian5huang/ngrok-sunny/master/sunny.py --tries=5 --timeout=5 -O ./ng_client.py
-python3 ng_client.py
